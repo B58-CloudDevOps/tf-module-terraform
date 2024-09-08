43e51ace86f8989db1ffed6dae18d54673873184
CodingManoj <CodingManoj@gmail.com>
2024-09-07 21:04:49 -0400

    AutoPush

--- data.tf
+++ data.tf
@@ -5,7 +5,8 @@ data "aws_route53_zone" "main" {
 
 data "aws_ami" "main" {
   most_recent = true
-  name_regex  = "DevOps-LabImage-RHEL9"
-  owners      = ["355449129696"]
+  # name_regex  = "DevOps-LabImage-RHEL9"
+  name_reges = "b58-golden-image"
+  owners     = ["355449129696"]
 }
 
