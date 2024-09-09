5fab405b03396338578128b7f0178c4c0d3221a1
CodingManoj <CodingManoj@gmail.com>
2024-09-08 21:47:55 -0400

    AutoPush

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "null_resource" "app" {
   provisioner "remote-exec" { # This let's the execution to happen on the remote node
     inline = [
       "pip3.11 install hvac",
-      "ansible-pull -U https://github.com/B58-CloudDevOps/ansible.git  -e COMPONENT=${var.name} -e ENV=${var.env} -e PWD=${var.pwd} expense-pull.yml"
+      "ansible-pull -U https://github.com/B58-CloudDevOps/ansible.git  -e COMPONENT=${var.name} -e ENV=${var.env} expense-pull.yml"
     ]
   }
 }
